b6cd6b34240e89fd1d1507a21613a26b56499c06
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-06-16 17:50:02 +0600

    Pass the  token to backend Server using middleware

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,4 +1,7 @@
 "use server";
+
+import { getUserToken } from "./session";
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const serverFetch = async (path) => {
@@ -11,6 +14,7 @@ export const serverMutation = async (path, data, method = 'POST') => {
         method: method,
         headers: {
             'Content-Type': 'application/json',
+            ...await authHeader(),
         },
         body: JSON.stringify(data),
     });
@@ -18,4 +22,12 @@ export const serverMutation = async (path, data, method = 'POST') => {
     //handle 401, 403, 500 errors here and throw appropriate messages for the client to display
 
     return res.json();
+}
+
+export const authHeader = async() => {
+    const token = await getUserToken();
+    const header = token ? {
+        authorization: `Bearer ${token}`,
+    } : {};
+    return header;
 }
